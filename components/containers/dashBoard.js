36cc0d52ea39dbe7f39e7b1021d4240ccf806f27
Chingsien <chingsien168@gmail.com>
2021-07-22 12:50:40 +0700

    new

--- components/containers/dashBoard.js
+++ components/containers/dashBoard.js
@@ -1,44 +1,51 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-    },
-    paper: {
-      width:260,
-      height:200,
-      margin:20,
-      padding: theme.spacing(2),
-      textAlign: 'center',
-      color: theme.palette.text.secondary,
-    },
-  }));
-  
-
-export default function Dashboard(){
-    const classes = useStyles();
-    return(
-        <>
- <div className={classes.root}>
-      <Grid container spacing={3}>
-        <Grid item xs>
-          <Paper className={classes.paper}>
-            <h3> New register</h3>
-          </Paper>
-        </Grid>
-        <Grid item xs>
-          <Paper className={classes.paper}>
-          <h3>All Order</h3></Paper>
-        </Grid>
-        <Grid item xs>
-          <Paper className={classes.paper}>
-            <h3>Total Sale</h3>
-          </Paper>
-        </Grid>
-      </Grid>
-    </div>
-        </>
-    )
-}
+// import React from 'react';
+// import { makeStyles } from '@material-ui/core/styles';
+// import { ResponsiveBump } from '@nivo/bump'
+// // make sure parent container have a defined height when using
+// // responsive component, otherwise height will be 0 and
+// // no chart will be rendered.
+// // website examples showcase many properties,
+// // you'll often use just a few of them.
+// const MyResponsiveBump = ({ data /* see data tab */ }) => (
+//     <ResponsiveBump
+//         data={data}
+//         margin={{ top: 40, right: 100, bottom: 40, left: 60 }}
+//         colors={{ scheme: 'spectral' }}
+//         lineWidth={3}
+//         activeLineWidth={6}
+//         inactiveLineWidth={3}
+//         inactiveOpacity={0.15}
+//         pointSize={10}
+//         activePointSize={16}
+//         inactivePointSize={0}
+//         pointColor={{ theme: 'background' }}
+//         pointBorderWidth={3}
+//         activePointBorderWidth={3}
+//         pointBorderColor={{ from: 'serie.color' }}
+//         axisTop={{
+//             tickSize: 5,
+//             tickPadding: 5,
+//             tickRotation: 0,
+//             legend: '',
+//             legendPosition: 'middle',
+//             legendOffset: -36
+//         }}
+//         axisRight={null}
+//         axisBottom={{
+//             tickSize: 5,
+//             tickPadding: 5,
+//             tickRotation: 0,
+//             legend: '',
+//             legendPosition: 'middle',
+//             legendOffset: 32
+//         }}
+//         axisLeft={{
+//             tickSize: 5,
+//             tickPadding: 5,
+//             tickRotation: 0,
+//             legend: 'ranking',
+//             legendPosition: 'middle',
+//             legendOffset: -40
+//         }}
+//     />
+// )
